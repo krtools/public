5697a47d4287dc8ea470369c59155b0239f81a48
Kyle Rector <krtools@users.noreply.github.com>
2025-11-10 13:29:57 -0500

    Create ResourcePool.ts

---
+++ ts/queues/ResourcePool.ts
@@ -0,0 +1,92 @@
+export interface ResourcePoolOptions<T> {
+  limit: number;
+  create: () => T | Promise<T>;
+  dispose: (item: T) => unknown;
+}
+
+export interface ResourceItem<T> {
+  value: T;
+  release: () => void;
+  [Symbol.dispose]: () => void;
+}
+
+export class ResourcePool<T> {
+  private options: ResourcePoolOptions<T>;
+
+  private closed = false;
+  private pool: T[] = [];
+  private total = 0;
+  private inUse = new Set<T>();
+
+  private queue: PromiseWithResolvers<T>[] = [];
+
+  constructor(opts: ResourcePoolOptions<T>) {
+    this.options = opts;
+  }
+
+  public get size() {
+    return this.pool.length;
+  }
+
+  public get running() {
+    return this.inUse.size;
+  }
+
+  private async _take() {
+    if (this.pool.length) {
+      const r = this.pool.pop()!;
+      this.inUse.add(r);
+      return r;
+    }
+
+    if (this.total < this.options.limit) {
+      this.total++;
+      const r = await this.options.create();
+      this.inUse.add(r);
+      return r;
+    }
+
+    const pw = Promise.withResolvers<T>();
+    this.queue.push(pw);
+    return pw.promise;
+  }
+
+  async _release(value: T) {
+    if (!this.inUse.has(value)) return;
+    this.inUse.delete(value);
+
+    if (this.closed) {
+      this.options.dispose(value);
+    } else if (this.queue.length) {
+      const next = this.queue.shift()!;
+      this.inUse.add(value);
+      next.resolve(value);
+    } else {
+      this.pool.push(value);
+    }
+  }
+
+  async acquire(): Promise<ResourceItem<T>> {
+    if (this.closed) throw new Error('ResourcePool is closed');
+
+    const value = await this._take();
+    const release = () => void this._release(value);
+
+    return {value, release, [Symbol.dispose]: release};
+  }
+
+  async close() {
+    this.closed = true;
+    // reject queued acquisitions
+    this.queue.forEach((pw) => pw.reject(new Error('ResourcePool closed')));
+
+    this.queue = [];
+    await Promise.all([...this.pool, ...this.inUse].map((r) => this.options.dispose(r)));
+    this.pool = [];
+    this.inUse.clear();
+  }
+
+  [Symbol.asyncDispose]() {
+    return this.close();
+  }
+}
