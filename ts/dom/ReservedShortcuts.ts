69517d6f188e628aa39eb36be2e1aa4d1ffc30c9
Kyle Rector <krtools@users.noreply.github.com>
2025-12-03 12:16:43 -0500

    Create ReservedShortcuts.ts

---
+++ ts/dom/ReservedShortcuts.ts
@@ -0,0 +1,72 @@
+export const RESERVED_SHORTCUTS = [
+  // --- Navigation / Tabs ---
+  "Ctrl+T","Cmd+T",
+  "Ctrl+W","Cmd+W",
+  "Ctrl+Shift+T","Cmd+Shift+T",
+  "Ctrl+Tab","Cmd+Tab",
+  "Ctrl+Shift+Tab","Cmd+Shift+Tab",
+  "Ctrl+1","Cmd+1","Ctrl+2","Cmd+2","Ctrl+3","Cmd+3",
+  "Ctrl+4","Cmd+4","Ctrl+5","Cmd+5","Ctrl+6","Cmd+6",
+  "Ctrl+7","Cmd+7","Ctrl+8","Cmd+8","Ctrl+9","Cmd+9",
+
+  // --- Window & App Control ---
+  "Ctrl+N","Cmd+N",
+  "Ctrl+Shift+N","Cmd+Shift+N",
+  "Ctrl+Q","Cmd+Q",
+  "Cmd+,", // Preferences
+  "Alt+F4",
+
+  // --- Refresh / Reload ---
+  "Ctrl+R","Cmd+R",
+  "Ctrl+Shift+R","Cmd+Shift+R","Cmd+Option+R",
+  "Ctrl+F5",
+
+  // --- Find / Search ---
+  "Ctrl+F","Cmd+F",
+  "Ctrl+G","Cmd+G",
+  "Ctrl+Shift+G","Cmd+Shift+G",
+  "Ctrl+L","Cmd+L",
+  "Ctrl+K","Cmd+K",
+
+  // --- Clipboard / Editing ---
+  "Ctrl+C","Cmd+C",
+  "Ctrl+X","Cmd+X",
+  "Ctrl+V","Cmd+V",
+  "Ctrl+Shift+V","Cmd+Shift+V",
+  "Ctrl+A","Cmd+A",
+  "Ctrl+Z","Cmd+Z",
+  "Ctrl+Y","Cmd+Y",
+  "Ctrl+Shift+Z","Cmd+Shift+Z",
+
+  // --- Dev Tools / Browser Features ---
+  "Ctrl+Shift+I","Cmd+Option+I","Cmd+Shift+I",
+  "Ctrl+Shift+C","Cmd+Option+C","Cmd+Shift+C",
+  "Ctrl+Shift+J","Cmd+Option+J","Cmd+Shift+J",
+  "F12",
+  "Ctrl+U","Cmd+U",
+  "Ctrl+S","Cmd+S",
+  "Ctrl+P","Cmd+P",
+  "Ctrl+O","Cmd+O",
+  "Ctrl+H","Cmd+H",
+  "Ctrl+J","Cmd+J",
+
+  // --- Zoom & View ---
+  "Ctrl+Plus","Cmd+Plus",
+  "Ctrl+-","Cmd+-",
+  "Ctrl+0","Cmd+0",
+  "F11","Cmd+Ctrl+F",
+
+  // --- System / OS Level ---
+  "Alt+Tab","Alt+Shift+Tab",
+  "Cmd+Tab","Cmd+Shift+Tab",
+  "Ctrl+Alt+Tab",
+  "Ctrl+Esc","Cmd+Esc",
+  "Ctrl+Alt+Del",
+  "Cmd+Option+Esc",
+  "Cmd+Space","Ctrl+Space",
+  "Win+R","Win+E","Win+D","Win+L","Win+Tab","Win+Shift+Tab",
+  "Cmd+H","Cmd+M", // Hide/minimize
+  "Cmd+Option+H","Cmd+Option+M",
+  "Cmd+Option+Tab",
+  "Cmd+Option+Q","Cmd+Shift+Q"
+];
